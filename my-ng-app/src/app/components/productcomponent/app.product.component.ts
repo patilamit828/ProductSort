003aa2bc8a1703eee28e8b0dcc379c94ee87051f
Amit Patil <amit.patil2@compucom.com>
2021-02-25 22:32:45 +0530

    Initial commit and Product Sort

--- my-ng-app/src/app/components/productcomponent/app.product.component.ts
+++ my-ng-app/src/app/components/productcomponent/app.product.component.ts
@@ -35,14 +35,65 @@ export class ProductComponent implements OnInit{  // the OnInit is implemented b
     clear():void {
       this.product = new ProductInfo(0, '', '', '', '','',0);
     }
+
     save():void {
+      //GetProductRowIds Array
+      let ProductIds = this.extractColumn(this.logic.getProducts(),'ProductRowId')
+      let lastProductId = Math.max(...ProductIds)
+      this.product.ProductRowId = lastProductId + 1;
       this.products = this.logic.addProduct(this.product);
+      this.clear();
     }
+
     getSelectedProduct(prd:ProductInfo):void {
       // this.product = prd;
       // use Object.assign(target, source);
       // target is an object in which source will be copied with its schema and values
       // but source and target are physicallyu different objects
       this.product = Object.assign({}, prd);
+    }
+
+    deleteRow(prd: ProductInfo):void {
+      this.products = this.products.filter((p)=>p.ProductRowId != prd.ProductRowId)
+    }
+
+    sort():void {
+      this.sortData(this.products, "ProductName","BasePrice");
+      //console.log(`After sorting: ${JSON.stringify(this.products)} `);
+
+    }
+
+    reverse():void {
+      console.log(`reverse works`)
+      this.sortData(this.products, "ProductRowId", "ProductRowId")
+    }
+
+    extractColumn (arr: Array<ProductInfo>, column: string): Array<number> {
+      return arr.map(x => x[column])
+    }
+
+    sortData (list: ProductInfo[], sortColumn1: any, sortColumn2: any) {
+      list.sort((a, b) => {
+        let sortaValue;
+        let sortbValue;
+
+        if (typeof(a[sortColumn1]) === 'string'){
+          sortaValue = a[sortColumn1].toUpperCase();
+          sortbValue = b[sortColumn1].toUpperCase();
+        }
+        else{
+          sortaValue = a[sortColumn1];
+          sortbValue = b[sortColumn2];
+        }
+
+        if (sortaValue > sortbValue) 
+          return 1
+        else if (sortaValue < sortbValue)
+          return -1
+        else
+          //Sorting of same product name >> by basePrice
+          return Number(a[sortColumn2]) > Number(b[sortColumn2]) ? 1: -1      
+        });
+      return list;
     }
 }
